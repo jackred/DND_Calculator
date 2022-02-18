55dfc54913c4068288c35d8d9e6fe54363b18804
jackred <jackred@tuta.io>
2022-02-18 16:11:48 +0000

    ADD all lv1 spell

--- Spells.py
+++ Spells.py
@@ -62,6 +62,7 @@ class Damage:
                 res[1][tmp] -= 1
                 res[1] = np.append(res[1], new_d[1])
                 res[0] = np.append(res[0], (i+i)+new_d[0])
+        print(res)
         return res[0], res[1] / res[1].sum()
 
     def is_hit(damage):
@@ -72,10 +73,13 @@ class Damage:
 
 
 class Spells:
-    def __init__(self, name, level, damages=[]):
+    def __init__(self, name, level, damages=[], range_s='touch',
+                 cast='action'):
         self.name = name
         self.level = level
         self.damages = damages
+        self.cast = cast
+        self.range_s = range_s
 
     def roll_on_hit(self, upcast=0, turn=0, rebound=0):
         return self.roll_all(Damage.is_hit, upcast, turn, rebound)
@@ -117,6 +121,31 @@ def test():
     chaos_bolt = Spells('Chaos Bolt', 1, [
         Damage(2, 8, 'Random', hit=True, upcast_per_level=0,
                rebound_spell=['self', cbd6], rebound=True), cbd6])
+    chromatic_orb = Spells('Chromatic Orb', 1, [
+        Damage(3, 8, 'Random', hit=True, upcast_per_level=1)])
+    dissonant_whispers = Spells('Dissonant Whispers', 1, [Damage(
+        3, 6, 'Psychic', dc=True, dc_type='WIS', dc_fail=0.5,
+        upcast_per_level=1)])
+    earth_tremor = Spells('Earth Tremor', 1, [Damage(
+        1, 6, 'Bludgeon', dc=True, dc_type='DEX', dc_fail=0,
+        upcast_per_level=1)])
+    frost_fingers = Spells('Frost Fingers', 1, [Damage(
+        2, 8, 'Ice', dc=True, dc_type='CON', dc_fail=0.5,
+        upcast_per_level=1)])
+    guiding_bolt = Spells('Guiding Bolt', 1, [
+        Damage(4, 6, 'Radiant', hit=True, upcast_per_level=1)])
+    hellish_rebuke = Spells('Hellish Rebuke', 1, cast='reaction', damages=[
+        Damage(4, 6, 'Fire',  dc=True, dc_type='DEX', upcast_per_level=1)])
+    inflict_wounds = Spells('Inflict Wound', 1, [
+        Damage(3, 10, 'Necrotic', hit=True, upcast_per_level=1)])
+    magic_missile = Spells('Magic Missile', 1, [
+        Damage(3, 4, 'Force', fix=1, hit=True, upcast_per_level=1)])
+    tashas_caustic_brew = Spells("Tasha's Caustic Brew", 1, [
+        Damage(2, 4, 'Acid', dc=True, dc_type='DEX', turn_roll=2,
+               upcast_per_level=2)])
+    thunderwave = Spells('Thunderwave', 1, [
+        Damage(2, 8, 'Thunder', dc=True, dc_type='CON', dc_fail=0.5,
+               upcast_per_level=1)])
     ax.plot(*burning_hands.roll_on_dc(upcast=0), label=burning_hands.name)
     ax.plot(*ice_knife.roll_all(upcast=0), label=ice_knife.name)
     ax.plot(*witch_bolt.roll_on_hit(upcast=0), label=witch_bolt.name)
@@ -125,8 +154,54 @@ def test():
     ax.plot(*catapult.roll_on_dc(upcast=0, turn=0), label=catapult.name)
     ax.plot(*arms_of_hadar.roll_on_dc(upcast=0, turn=2),
             label=arms_of_hadar.name)
+    ax.plot(*chaos_bolt.roll_on_hit(upcast=0, rebound=0),
+            label=chaos_bolt.name)
     ax.plot(*chaos_bolt.roll_on_hit(upcast=0, rebound=1),
             label=chaos_bolt.name + ' 1 rebound')
+    ax.plot(*chromatic_orb.roll_on_hit(upcast=0),
+            label=chromatic_orb.name)
+    ax.plot(*dissonant_whispers.roll_on_dc(upcast=0),
+            label=dissonant_whispers.name)
+    ax.plot(*earth_tremor.roll_on_dc(upcast=0),
+            label=earth_tremor.name)
+    ax.plot(*frost_fingers.roll_on_dc(upcast=0),
+            label=frost_fingers.name)
+    ax.plot(*guiding_bolt.roll_on_hit(upcast=0),
+            label=guiding_bolt.name)
+    ax.plot(*hellish_rebuke.roll_on_dc(upcast=0),
+            label=hellish_rebuke.name)
+    ax.plot(*inflict_wounds.roll_on_hit(upcast=0),
+            label=inflict_wounds.name)
+    ax.plot(*magic_missile.roll_on_hit(upcast=0),
+            label=magic_missile.name)
+    ax.plot(*tashas_caustic_brew.roll_on_dc(upcast=0),
+            label=tashas_caustic_brew.name)
+    ax.plot(*tashas_caustic_brew.roll_on_dc(upcast=0, turn=2),
+            label=tashas_caustic_brew.name+' 2 turn')
+    ax.plot(*thunderwave.roll_on_dc(upcast=0),
+            label=thunderwave.name)
+    ax.legend()
+    plt.show()
+
+
+def lv3():
+    fig, ax = plt.subplots()
+    ax.set_title('DND lv 3spell damage comparison')
+    ax.set_ylabel('percentage')
+    ax.set_xlabel('damage')
+    fireball = Spells('Fireball', 3, [Damage(
+        8, 6, 'Fire', dc=True, dc_type='DEX', dc_fail=0.5,
+        upcast_per_level=1)])
+    cbd6_hit = Damage(3, 6, 'Random', hit=True, upcast_per_level=1)
+    cbd8 = Damage(2, 8, 'Random', hit=True, upcast_per_level=0,
+                  rebound_spell=['self', cbd6_hit], rebound=True)
+    chaos_ball = Spells('Chaos Ball', 3, [
+        Damage(2, 20, 'Random', dc=True, dc_type='DEX', upcast_per_level=0,
+               rebound_spell=[cbd8, cbd6_hit], rebound=True),
+        Damage(1, 6, 'Random', dc=True, dc_type='DEX', upcast_per_level=1)])
+    ax.plot(*fireball.roll_on_dc(upcast=0), label=fireball.name)
+    ax.plot(*chaos_ball.roll_on_dc(upcast=0, rebound=1),
+            label=chaos_ball.name)
     ax.legend()
     plt.show()
 
